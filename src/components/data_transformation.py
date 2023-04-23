c3f476ed925e8b37fbde56c72e905e953daf6b0f
Dinesh <dineshsairam9394@gmail.com>
2023-04-23 17:14:05 +0530

    Model Training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -106,7 +106,6 @@ class DataTransformation:
 
             train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
             test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
-
             logging.info(f"Saved preprocessing Object.")
 
             save_object(
